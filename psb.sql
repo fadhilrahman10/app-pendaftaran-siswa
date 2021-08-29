1a0e7934119a595d939210a0a9be8056ec253a60
fadhil rahman <fadhilrahman920@gmail.com>
2021-08-29 21:24:59 +0700

    memperbaiki db

--- psb.sql
+++ psb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 27 Agu 2021 pada 20.38
+-- Waktu pembuatan: 29 Agu 2021 pada 16.24
 -- Versi server: 10.4.20-MariaDB
 -- Versi PHP: 7.4.22
 
@@ -32,62 +32,51 @@ CREATE TABLE `admin` (
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `nama_admin` varchar(255) DEFAULT NULL,
-  `role_user` varchar(255) DEFAULT NULL,
-  `id_user` int(255) DEFAULT NULL
+  `role_user` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `admin`
 --
 
-INSERT INTO `admin` (`Id`, `email`, `password`, `nama_admin`, `role_user`, `id_user`) VALUES
-(23, 'admin@gmail.com', '$2y$10$XfwL6NlulqGMqGNtMIBFieoJ/Ot0ZX4t529rVqwNiEopGNi.pJkli', 'Rio Anggiri', '0', NULL),
-(27, 'andretaulani@gmail.com', '$2y$10$2ZEOwfc/F0nZore3IyYOVu7pzv3mrNbqOvFE3gCnz7pjeghVEU6g6', '', '1', 15);
+INSERT INTO `admin` (`Id`, `email`, `password`, `nama_admin`, `role_user`) VALUES
+(23, 'admin@gmail.com', '$2y$10$XfwL6NlulqGMqGNtMIBFieoJ/Ot0ZX4t529rVqwNiEopGNi.pJkli', 'Rio Anggiri', '0'),
+(30, 'rioanggara@gmail.com', '$2y$10$WkNfR5dX/J.jEAcFhXkq9.QM7sD3fTw55iSd5ykz4cDYsUPpUBB6W', 'Rio Anggara', '1');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pendaftaran`
+-- Struktur dari tabel `siswa`
 --
 
-CREATE TABLE `pendaftaran` (
+CREATE TABLE `siswa` (
   `Id` int(11) NOT NULL,
+  `nis` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `nama` varchar(100) DEFAULT NULL,
   `nama_panggilan` varchar(50) DEFAULT NULL,
   `tempat_lahir` varchar(50) DEFAULT NULL,
   `tanggal_lahir` date DEFAULT NULL,
   `jenis_kelamin` char(1) DEFAULT NULL,
+  `agama` varchar(100) NOT NULL,
   `anak_ke` int(11) DEFAULT NULL,
   `jumlah_saudara` int(11) DEFAULT NULL,
   `alamat` varchar(255) DEFAULT NULL,
-  `nama_ayah` varchar(255) DEFAULT NULL,
-  `tempat_lahir_ayah` varchar(255) DEFAULT NULL,
-  `tanggal_lahir_ayah` date DEFAULT NULL,
-  `pendidikan_terakhir_ayah` varchar(50) DEFAULT NULL,
-  `pekerjaan_ayah` varchar(255) DEFAULT NULL,
-  `agama_ayah` varchar(255) DEFAULT NULL,
-  `nama_ibu` varchar(255) DEFAULT NULL,
-  `tempat_lahir_ibu` varchar(255) DEFAULT NULL,
-  `tanggal_lahir_ibu` date DEFAULT NULL,
-  `pendidikan_terakhir_ibu` varchar(255) DEFAULT NULL,
-  `pekerjaan_ibu` varchar(255) DEFAULT NULL,
-  `agama_ibu` varchar(255) DEFAULT NULL,
   `telp` varchar(255) DEFAULT NULL,
   `upload_akte` varchar(255) DEFAULT NULL,
   `upload_kartu_keluarga` varchar(255) DEFAULT NULL,
   `foto_anak` varchar(255) DEFAULT NULL,
-  `foto_keluarga` varchar(255) DEFAULT NULL,
   `upload_ijazah` varchar(255) NOT NULL,
   `upload_skl` varchar(255) NOT NULL,
   `tanggal_daftar` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pendaftaran`
+-- Dumping data untuk tabel `siswa`
 --
 
-INSERT INTO `pendaftaran` (`Id`, `nama`, `nama_panggilan`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `anak_ke`, `jumlah_saudara`, `alamat`, `nama_ayah`, `tempat_lahir_ayah`, `tanggal_lahir_ayah`, `pendidikan_terakhir_ayah`, `pekerjaan_ayah`, `agama_ayah`, `nama_ibu`, `tempat_lahir_ibu`, `tanggal_lahir_ibu`, `pendidikan_terakhir_ibu`, `pekerjaan_ibu`, `agama_ibu`, `telp`, `upload_akte`, `upload_kartu_keluarga`, `foto_anak`, `foto_keluarga`, `upload_ijazah`, `upload_skl`, `tanggal_daftar`) VALUES
-(15, 'Rio Anggiri', 'Rio', 'Tapung', '2009-01-01', 'L', 1, 2, 'Jl. Hang Tuah', 'Rahmad', 'Medan', '1980-01-01', 'SMK', 'Karyawan Swasta', 'Islam', 'Nadia', 'Bandung', '1980-01-01', 'SMA', 'IRT', 'Islam', '081236463332', '', '', '', '', '', '', '2021-08-27 13:25:59');
+INSERT INTO `siswa` (`Id`, `nis`, `email`, `nama`, `nama_panggilan`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `agama`, `anak_ke`, `jumlah_saudara`, `alamat`, `telp`, `upload_akte`, `upload_kartu_keluarga`, `foto_anak`, `upload_ijazah`, `upload_skl`, `tanggal_daftar`) VALUES
+(18, '100500', 'rioanggara@gmail.com', 'Rio Anggara', 'Rio', 'Tapung', '2009-01-01', 'L', 'Islam', 1, 2, 'Jl. Hang Tuah', '081236463332', '03-08-39user-03.jpg', '03-08-39user-04.jpg', '04-08-22user-12.jpg', '04-08-55user-05.jpg', '04-08-55user-07.jpg', '2021-08-29 14:22:22');
 
 --
 -- Indexes for dumped tables
@@ -97,14 +86,12 @@ INSERT INTO `pendaftaran` (`Id`, `nama`, `nama_panggilan`, `tempat_lahir`, `tang
 -- Indeks untuk tabel `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`Id`),
-  ADD KEY `id_user` (`id_user`),
-  ADD KEY `id_user_2` (`id_user`);
+  ADD PRIMARY KEY (`Id`);
 
 --
--- Indeks untuk tabel `pendaftaran`
+-- Indeks untuk tabel `siswa`
 --
-ALTER TABLE `pendaftaran`
+ALTER TABLE `siswa`
   ADD PRIMARY KEY (`Id`);
 
 --
@@ -115,23 +102,13 @@ ALTER TABLE `pendaftaran`
 -- AUTO_INCREMENT untuk tabel `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
--- AUTO_INCREMENT untuk tabel `pendaftaran`
+-- AUTO_INCREMENT untuk tabel `siswa`
 --
-ALTER TABLE `pendaftaran`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `admin`
---
-ALTER TABLE `admin`
-  ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `pendaftaran` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `siswa`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
